81359546c85b5b16115638050a42c6104c6db26e
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-01-06 13:43:52 +0100

    Modifukacja skryptu

--- teachers_materials/spotkanie_2/06_if_else.py
+++ teachers_materials/spotkanie_2/06_if_else.py
@@ -1,22 +1,21 @@
 actual_year = 2021
+birth = 1974
+my_name = "Adam"
 
-print("Hi, we'll try to compute your age ;-)")
-name = input("Please tell me your name:")
-birth = input(f"OK {name} - at what year have you birth?")
-birth = int(birth)
+print(f"Hi {my_name}, we'll try to compute your age ;-)")
 age = actual_year - birth
 
 if age >= 18:
-    print(f"Oh, I see, {name} - you are an adult now.")
+    print(f"Oh, I see, {my_name} - you are an adult now.")
     print(f"You are {age} years old.")
 else:
     print(f"You are young - {age} years old.")
 
 
 if name.endswith("a"):
-    print(f"I guess {name} - you are a woman.")
+    print(f"I guess {my_name} - you are a woman.")
     if name.lower() == "barnaba":
-        print(f"But your name: {name} is an exception - you are a man!")
+        print(f"But your name: {my_name} is an exception - you are a man!")
 else:
     print("You probably are a man...")
 print("That is all - see you next time!")
