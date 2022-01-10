4adc5058296723623d126df6fe0fcecd6bdbb6ac
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-01-10 15:41:41 +0100

    Aktualizacja spotkanie_2

--- teachers_materials/spotkanie_2/02_listy_slowniki.py
+++ teachers_materials/spotkanie_2/02_listy_slowniki.py
@@ -1,71 +1,71 @@
 # definiujemy listę - kolekcję elementów, które zachowują kolejność wprowadzania
 print("---[LISTA / list]-------------------------------------------------------")
-addressbook = [
-    "Duda Andrzej",
-    "Kornhauser-Duda Agata",
-    "Witek Elżbieta",
-    "Czarzasty Włodzimierz",
-    "Gosiewska Małgorzata",
-    "Kidawa-Błońska Małgorzata",
-    "Terlecki Ryszard",
-    "Zgorzelski Piotr",
+planes = [
+    "Boeing 737-300",
+    "Airbus A320",
+    "Boeing 777-300ER",
+    "Boeing 787 Dreamliner",
+    "Airbus A330",
+    "Boeing 747 Jumbo Jet",
+    "Airbus A380",
+    "Bombardier CRJ-900",
 ]
 
 # odwołujemy się do poszczególnych elementów
-print(f"Pełna lista: {addressbook}")
-print(addressbook[0])
-print(addressbook[3])
-print(addressbook[5])
-print(addressbook[-1])  # ostatni - liczymy indeksy ujemnie
+print(f"Pełna lista: {planes}")
+print(planes[0])
+print(planes[3])
+print(planes[5])
+print(planes[-1])  # ostatni - liczymy indeksy ujemnie
 
 # sprawdzamy ilość elementów
-print(len(addressbook))
+print(len(planes))
 
 # sprawdzamy czy element istnieje w liście
-print("Duda Andrzej" in addressbook)
-print("Kaczyński Jarosław" in addressbook)
+print("Boeing 777-300ER" in planes)
+print("Bombardier Dash 8" in planes)
 
 # lista potrafi być modyfikowalna w miejscu (ang. 'mutable')
-print(f"Przed sortowaniem: {addressbook}")
-addressbook.sort()
-print(f"Po sortowaniu: {addressbook}")
+print(f"Przed sortowaniem: {planes}")
+planes.sort()
+print(f"Po sortowaniu: {planes}")
 
 print("---[SŁOWNIK / dict]-------------------------------------------------------")
 # definiujemy słownik - kolekcję elementów o strukturze klucz:wartość
 # które nie muszą zachowywać kolejności
-addressbook = {
-    "Prezydent": "Duda Andrzej",
-    "Żona Prezydenta": "Kornhauser-Duda Agata",
-    "Marszałek Sejmu": "Witek Elżbieta",
-    "Wce - Lewica": "Czarzasty Włodzimierz",
-    "Wce - PiS 1": "Gosiewska Małgorzata",
-    "Wce - KO": "Kidawa-Błońska Małgorzata",
-    "Wce - PiS 2": "Terlecki Ryszard",
-    "Wce - KO/PSL": "Zgorzelski Piotr",
+planes = {
+    "Airbus A380": 850,
+    "Boeing 737-300": 188,
+    "Boeing 787 Dreamliner": 330,
+    "Airbus A320": 180,
+    "Boeing 777-300ER": 550,
+    "Bombardier CRJ-900": 90,
+    "Boeing 747 Jumbo Jet": 550,
+    "Airbus A330": 300,
 }
 
 # odwołujemy się do poszczególnych elementów
-print(f"Pełny słownik: {addressbook}")
-print(addressbook["Prezydent"])
-print(addressbook["Marszałek Sejmu"])
-print(addressbook["Wce - KO"])
+print(f"Pełny słownik: {planes}")
+print(planes["Airbus A320"])
+print(planes["Boeing 747 Jumbo Jet"])
+print(planes["Boeing 737-300"])
 # aby działało dobrze, trzeba zakomentować poniższą linię
-print(addressbook[-1])  # ostatni - ERROR !!!
+print(planes[-1])  # ostatni - ERROR !!!
 # Traceback (most recent call last):
 #   File "02_listy_slowniki.py", line 53, in <module>
-#     print(addressbook[-1])  # ostatni - ERROR !!!
+#     print(planes[-1])  # ostatni - ERROR !!!
 # KeyError: -1
 
 
 # sprawdzamy ilość elementów
-print(len(addressbook))
+print(len(planes))
 
 # sprawdzamy czy element istnieje w słowniku - szukamy tak tylko wśród kluczy
-print("Duda Andrzej" in addressbook)
-print("Prezydent" in addressbook)
+print("Boeing 737-300" in planes)
+print("Boeing 737-310" in planes)
 
 # możemy otrzymać elementy kluczy jako listę
-print(addressbook.keys())
+print(planes.keys())
 
 # możemy otrzymać elementy wartości jako listę
-print(addressbook.values())
+print(planes.values())
--- teachers_materials/spotkanie_2/05_for_while.py
+++ teachers_materials/spotkanie_2/05_for_while.py
@@ -1,29 +1,31 @@
-addressbook = [
-    "Duda Andrzej",
-    "Kornhauser-Duda Agata",
-    "Witek Elżbieta",
-    "Czarzasty Włodzimierz",
-    "Gosiewska Małgorzata",
-    "Kidawa-Błońska Małgorzata",
-    "Terlecki Ryszard",
-    "Zgorzelski Piotr",
+planes = [
+    "Boeing 737-300",
+    "Airbus A320",
+    "Boeing 777-300ER",
+    "Boeing 787 Dreamliner",
+    "Airbus A330",
+    "Boeing 747 Jumbo Jet",
+    "Airbus A380",
+    "Bombardier CRJ-900",
 ]
 
-print(f"Pełna lista: {addressbook}")
+print(f"Pełna lista: {planes}")
 
 print("---[Pętla iteracyjna for .. in _kolekcja_ ...]------")
 # pętla iteracyjna - wykonuje się tyle razy, ile elementów posiada kolekcja
-for name in addressbook:
-    print(f"Osoba: {name}")
+for plane in planes:
+    print(f"Samolot: {plane}")
     # przykład wykorzystania wyrażenia logicznego w {}
     # zwracamy uwagę na różne znaki cudzysłowów
-    print(f"Czy o nazwisku Duda? -> {'Duda' in name}")
+    print(f"Czy to jest Boeing? -> {'Boeing' in plane}")
+    print(f"Czy instnieje Boeing 747 Jumbo Jet? -> {'Boeing 747 Jumbo Jet' in plane}")
 
 print("---[Pętla warunkowa while _warunek_ ...]------")
 # pętla warunkowa - wykonuje się tak długo, jak długo warunek jest prawdziwy
-# w naszym wypadku element listy inny niż "Terlecki Ryszard"
+# w naszym wypadku element listy inny niż "Boeing 747 Jumbo Jet"
 indeks = 0
-while addressbook[indeks] != "Terlecki Ryszard":
-    print(f"Szukamy Pana Terleckiego, a w liście jest: {addressbook[indeks]}")
+while planes[indeks] != "Boeing 747 Jumbo Jet":
+    print(f"Szukamy samolotu Boeing 747 Jumbo Jet, a w liście jest: {planes[indeks]} / indeks = {indeks}")
     # zwiększamy indeks
     indeks += 1
+print(f"Znaleźliśmy {planes[indeks]} / indeks = {indeks}")
