d8e01875f55ff9405d4453d23333c89b265c2a39
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-01-28 11:21:42 +0100

    f-string

--- teachers_materials/spotkanie_2/01_podstawowe_typy.py
+++ teachers_materials/spotkanie_2/01_podstawowe_typy.py
@@ -6,10 +6,13 @@ print("To jest dowolna informacja wyświetlona na ekranie.")
 
 # definiujemy zmienne - typy podstawowe
 # nazwy zmiennych zapisujemy małymi literami, oddzielając słowa znakami podkreślenia
-language = "Python"     # str   - ciąg znaków
-year_of_birth = 1991    # int   - liczba całkowita
-value_pi = 3.1415927    # float - liczba rzeczywista, zmiennoprzecinkowa
-python_is_cool = True   # bool  - wartość logiczna (prawda/fałsz)
+language = "Python"  # str   - ciąg znaków
+year_of_birth = 1991  # int   - liczba całkowita
+value_pi = 3.1415927  # float - liczba rzeczywista, zmiennoprzecinkowa
+python_is_cool = True  # bool  - wartość logiczna (prawda/fałsz)
+
+# F-string opisany jest w PEP-498: https://www.python.org/dev/peps/pep-0498/
+info = f"The language {language} was made in {year_of_birth} year."
 
 print("Hi, I want to compute some things!")
 # obliczenia - wyrażenia arytmetyczne
@@ -17,5 +20,5 @@ circle_radius = 6
 circle_area = pi * circle_radius ** 2
 
 # wypisujemy na ekranie stosując konwencję F-String
-# F-string opisany jest w PEP-498: https://www.python.org/dev/peps/pep-0498/
+print(info)
 print(f"Area is {circle_area}, and {language} was born in {year_of_birth}.")
